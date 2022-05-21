809c993baced6eafb2af73a8176760c43a6c5239
Michael Terletskyi <michaelterletskyiofficial@gmail.com>
2022-05-21 12:26:18 +0300

    104. Maximum Depth of Binary Tree - has been resolved on Java

---
+++ Easy/104. Maximum Depth of Binary Tree/README.md
@@ -0,0 +1,3 @@
+Level : Easy
+
+https://leetcode.com/problems/maximum-depth-of-binary-tree/
--- Easy/94. Binary Tree Inorder Traversal/solutions/Solution94.java
+++ Easy/94. Binary Tree Inorder Traversal/solutions/Solution94.java
@@ -1,22 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-
 public class Solution94 {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
