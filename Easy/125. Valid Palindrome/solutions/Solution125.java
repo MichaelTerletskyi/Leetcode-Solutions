06093d23771879fa9fea9b1d57954ab142ff43ec
Michael Terletskyi <michaelterletskyiofficial@gmail.com>
2022-05-24 12:42:38 +0300

    125. Valid Palindrome - has been resolved on Java (FIX)

--- Easy/125. Valid Palindrome/solutions/Solution125.java
+++ Easy/125. Valid Palindrome/solutions/Solution125.java
@@ -1,6 +1,7 @@
 public class Solution125 {
     public boolean isPalindrome(String s) {
-        int i = 0, j = s.length() - 1;
+        int i = 0;
+        int j = s.length() - 1;
         while (i < j) {
             if (!Character.isLetterOrDigit(s.charAt(i))) {
                 i++;
